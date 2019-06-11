2299a7a3d1e134131da77ad53d667349024a34e3
MicWes <micwesley@gmail.com>
2019-06-10 21:16:15 -0300

    beans e inserts no banco

---
+++ src/java/sql/inserts_mutantes.sql
@@ -0,0 +1,23 @@
+﻿insert into Usuario(login, password)
+values ('ana', '1234');
+
+insert into Usuario(login, password)
+values ('arthur', 'aaaa');
+
+insert into Usuario(login, password)
+values ('michael', '5678');
+
+insert into Habilidade(description)
+values ('lançador de fogo');
+
+insert into Habilidade(description)
+values ('teleporte');
+
+insert into Habilidade(description)
+values ('mudança de aparência');
+
+insert into Habilidade(description)
+values ('leitura de mentes');
+
+insert into Habilidade(description)
+values ('regeneração rápida');
