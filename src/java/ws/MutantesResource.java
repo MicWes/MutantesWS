29dccb40a763c0035958756ea750ec701ebe6ef2
MicWes <micwesley@gmail.com>
2019-06-12 18:22:47 -0300

    atualizando metodos get

--- src/java/ws/MutantesResource.java
+++ src/java/ws/MutantesResource.java
@@ -5,6 +5,9 @@
  */
 package ws;
 
+import mb.Mutante;
+import mb.Usuario;
+import mb.Habilidade;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
@@ -12,6 +15,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -41,6 +46,19 @@ public class MutantesResource {
         //TODO return proper representation object
         throw new UnsupportedOperationException();
     }
+    
+    @GET
+    @Path("/usuario")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Usuario getUsuario(@QueryParam("login") String login, @QueryParam("password") String password){
+        //query select usuario
+        Usuario u = new Usuario();
+        u.setUser_id(1); //id do banco
+        u.setLogin("ana"); //login do banco
+        u.setPassword("1234"); //password do banco
+        return u;
+        //lado cliente pag 84 dos slides
+    }
 
     /**
      * PUT method for updating or creating an instance of MutantesResource
@@ -50,4 +68,16 @@ public class MutantesResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public void putJson(String content) {
     }
+    
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Mutante insertMutante(Mutante m) {
+        m.setNome("Noturno");
+        m.setUser_id(1);
+        m.setHability_id(1);
+        //m.setImage();
+        return m;
+    }
+   
 }
